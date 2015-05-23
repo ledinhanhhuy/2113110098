30c950350bed973d09b363ff55ba7b36db8b3723
Admin <Admin@D20909>
2015-05-23 14:32:42 +0700

    gfgd

---
+++ example02/src/example02/Main.java
@@ -0,0 +1,13 @@
+package example02;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		Rectangle recta= new Rectangle(3,4);
+		Rectangle rectb= new Rectangle(5,6);
+		System.out.println("rect a:"+recta.area());
+		System.out.println("rect b:"+rectb.area());
+	}
+
+}
